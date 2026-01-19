cba5e853d35881f4f77c1e251008579939e4aaaf
Ariful <arifulmit17@gmail.com>
2026-01-19 22:29:51 +0600

    using fetch

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,7 +1,20 @@
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { cookies } from "next/headers";
 
 
-export default function Home() {
+export default async function Home() {
+  const session= await authClient.getSession()
+  const cookieStore= await cookies()
+  console.log(cookies);
+
+  const res=await fetch("http://localhost:3000/api/auth/get-session",{
+    headers:{
+      Cookie:cookieStore.toString()
+    }
+  })
+  const sess= res.json()
+  console.log(sess );
   return (
     <div >
       <Button>Click here</Button>
