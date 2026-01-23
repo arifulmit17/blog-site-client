809bcb678d872e97aeb6eb1eb410f6258f5ed138
Ariful <arifulmit17@gmail.com>
2026-01-23 19:28:27 +0600

    added env

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,20 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
+import { userService } from "@/services/user.service";
 import { cookies } from "next/headers";
 
 
 export default async function Home() {
-  const session= await authClient.getSession()
-  const cookieStore= await cookies()
-  console.log(cookies);
-
-  const res=await fetch("http://localhost:3000/api/auth/get-session",{
-    headers:{
-      Cookie:cookieStore.toString()
-    }
-  })
-  const sess= res.json()
-  console.log(sess );
+  const {data}= await userService.getSession()
+  console.log(data);
   return (
     <div >
       <Button>Click here</Button>
