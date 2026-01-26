407c491051acb4f6436fd5f4ce86aa35fd37fe26
Ariful <arifulmit17@gmail.com>
2026-01-26 11:33:34 +0600

    added login form

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -1,13 +1,33 @@
-import React from 'react'
-export const dynamic = 'force-dynamic'
-// 'auto' | 'force-dynamic' | 'error' | 'force-static'
+"use client";
 
-export default async function AboutPage() {
-    await new Promise((resolve)=>setTimeout(resolve,4000))
 
-    // throw new Error("Something went wrong")
+import { getBlogs } from "@/actions/blog.actions";
+import { useEffect, useState } from "react";
+
+export default function AboutPage() {
+  const [data, setData] = useState();
+  const [error, setError] = useState<{ message: string } | null>(null);
+
+  console.log(data);
+  console.log(error);
+  useEffect(() => {
+    (async () => {
+      const { data, error } = await getBlogs();
+
+      setData(data);
+      setError(error);
+    })();
+  }, []);
+
   return (
-    <div >
-        <h1>About page</h1></div>
-  )
+    <div>
+      <h1> This is about page component </h1>
+    </div>
+  );
 }
+
+//* For simulating load time
+// await new Promise((resolve) => setTimeout(resolve, 4000));
+
+//* For simulating error
+// throw new Error("Something went wrong");
